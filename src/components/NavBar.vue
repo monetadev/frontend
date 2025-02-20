ae0c8ac16b9d81fe2a42aaf2706141a86e65f568
Chris <marac@farmingdale.edu>
2025-02-20 16:51:57 -0500

    Added team name to the left of the nav bar. Added my name and email and style for team name

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
   <nav class="navbar">
-    <div class = "team name">
+
+    <div class = "team-name">
       <h1>Moneta</h1>
     </div>
 
@@ -31,10 +32,11 @@
             <path d="M234-276q51-39 114-61.5T480-360q69 0 132 22.5T726-276q35-41 54.5-93T800-480q0-133-93.5-226.5T480-800q-133 0-226.5 93.5T160-480q0 59 19.5 111t54.5 93Zm246-164q-59 0-99.5-40.5T340-580q0-59 40.5-99.5T480-720q59 0 99.5 40.5T620-580q0 59-40.5 99.5T480-440Zm0 360q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q53 0 100-15.5t86-44.5q-39-29-86-44.5T480-280q-53 0-100 15.5T294-220q39 29 86 44.5T480-160Zm0-360q26 0 43-17t17-43q0-26-17-43t-43-17q-26 0-43 17t-17 43q0 26 17 43t43 17Zm0-60Zm0 360Z"/>
           </svg>
         </li>
-        <li><a href="#">About</a>
-        <p>chris</p></li>
+        <li><a href="#">Christopher Maradiaga</a>
+        <p>marac@farmingdale.edu</p></li>
         <li class =" drop-down"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-360 280-560h400L480-360Z"/></svg></li>
       </ul>
+
     </div>
   </nav>
 </template>
@@ -46,6 +48,13 @@ export default {
 </script>
 
 <style scoped>
+.team-name h1 {
+  font-size: 2rem; /* Adjust the size as needed */
+  font-family: 'Arial', sans-serif; /* Choose your preferred font family */
+  color: white; /* Change color to a green shade or any other */
+  text-align: center; /* Align the text */
+  font-weight: bold; /* Make the text bold */
+}
 .navbar {
   position: fixed;
   top: 0;

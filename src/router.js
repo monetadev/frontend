c81ea982c530a131114117acd01b074b9beb3655
Gagan <gagansapkota668@gmail.com>
2025-02-24 13:11:16 -0500

    Changed the code so that the localhost defaults to the home page

--- src/router.js
+++ src/router.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import Home from "./components/Home.vue";
 import ResetPassword from './components/ResetPassword.vue'; // Update path
 
 const routes = [
-  { path: '/', redirect: '/reset-password' },
+  { path: "/", redirect: "/home" },
+  { path: "/home", component: Home },
   { path: '/reset-password', component: ResetPassword }
 ];
 

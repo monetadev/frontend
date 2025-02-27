2e62014afa8fa464776cf7af656c0206c572f6c4
Chris M <maradiagaChris03@gmail.com>
2025-02-27 15:04:04 -0500

    making circle component single component and adding scripts

--- src/components/ProgressCircle.vue
+++ src/components/ProgressCircle.vue
@@ -1,11 +1,24 @@
 <template>
+  <div class="progress circle" :class="{active: isActive}">
+    <span class = "number">{{ number }}</span>
+  </div>
 
 </template>
 
 <script>
 export default {
+  props: {
+    number: {
+      type: Number,
+      required: true
+    },
+    isActive: {
+      type: Boolean,
+      default: false
+    }
+  }
 
-}
+};
 </script>
 
 <style scoped>

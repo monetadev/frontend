3bf468cae20eb2bd1af62000e9d75ec6730dfd32
Chris M <maradiagaChris03@gmail.com>
2025-02-27 15:10:31 -0500

    Created ProgressLine.vue

---
+++ src/components/ProgressLine.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="progress-line" :class="{active: isActive}"></div>
+</template>
+
+<script>
+export default {
+  name: "ProgressLine"
+}
+</script>
+<style scoped>
+
+</style>

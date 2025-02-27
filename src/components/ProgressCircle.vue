c47ca9d3bf3df135fc0808d948acb786a17d2f90
Chris M <maradiagaChris03@gmail.com>
2025-02-27 15:07:24 -0500

    added my style

--- src/components/ProgressCircle.vue
+++ src/components/ProgressCircle.vue
@@ -23,5 +23,24 @@ export default {
 
 <style scoped>
 
+.progress-circle {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #333;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color:white;
+  font-size: 24px;
+  font-weight: bold;
+  transition: background-color 0.3s ease;
+}
+.progress-circle.active {
+  background-color:#5F98EF;
+}
+.number {
+  font-size: 16px;
+}
 
 </style>

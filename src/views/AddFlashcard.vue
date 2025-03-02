6ef95ae6d4142e2977a7cdd5cc6c8110935e6f0f
jomal <johnj18@farmingdale.edu>
2025-03-02 15:30:23 -0500

    - Added Sidebar Navigation and Top Navigation Bar to AddFlashcard.vue

--- src/views/AddFlashcard.vue
+++ src/views/AddFlashcard.vue
@@ -1,7 +1,34 @@
 <template>
+  <div class="dashboard">
+    <SidebarNavigation :isCollapsed="isSidebarCollapsed" @toggle="toggleSidebar" />
+    <div class="main-content" :class="{ 'collapsed': isSidebarCollapsed }">
+      <NavigationBar :isSidebarCollapsed="isSidebarCollapsed" />
+
+    </div>
+  </div>
 </template>
 
 <script>
+import SidebarNavigation from "@/components/SideNavigation.vue";
+import NavigationBar from "@/components/TopNavigation.vue";
+
+export default {
+  name: "AddFlashcard",
+  components: {
+    SidebarNavigation,
+    NavigationBar,
+  },
+  data() {
+    return {
+      isSidebarCollapsed: false,
+    };
+  },
+  methods: {
+    toggleSidebar() {
+      this.isSidebarCollapsed = !this.isSidebarCollapsed;
+    },
+  },
+};
 </script>
 <style scoped>
 </style>

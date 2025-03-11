c2fc9e41b76234d7ad41d13da7f50e440258b13e
Gagan <gagansapkota668@gmail.com>
2025-03-11 12:25:34 -0400

    added router for the settings page

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Login from './views/Login.vue';
 import createAccount  from './views/createAccount.vue';
 import AddFlashcard  from './views/AddFlashcard.vue';
 import flash  from './components/GeneratedFlashcard.vue';
+import Settings from './views/Settings.vue';
 
 const routes = [
   { path: '/dashboard', component: Dashboard },
@@ -12,7 +13,8 @@ const routes = [
   { path: '/reset-password', component: ResetPassword },
   {path: '/signup', component: createAccount},
   {path: '/add-flashcard', component: AddFlashcard},
-  {path: '/flash', component: flash}
+  {path: '/flash', component: flash},
+  { path: '/settings', component: Settings}
 ];
 
 const router = createRouter({

c67b2cd5af54c2ac0124edceba031171e3247dc1
Chris <marac@farmingdale.edu>
2025-03-26 14:05:01 -0400

    got routing issue to work

--- src/router.js
+++ src/router.js
@@ -7,8 +7,8 @@ import AddFlashcard  from './views/AddFlashcard.vue';
 import Settings from './views/Settings.vue';
 import Set from './views/flashsetViewer.vue';
 import View from './views/flashcardViewer.vue';
-
-=======
+import Create from './views/createSet.vue';
+import Quizzes from './views/Quizzes.vue';
 
 
 const routes = [
@@ -18,16 +18,25 @@ const routes = [
   {path: '/signup', component: createAccount},
   {path: '/add-flashcard', component: AddFlashcard},
   { path: '/quizzes', component: Quizzes},
-  {path: '/my-stuffs', component: Set},
+  {path: '/my-stuffs', component: Set, exact : true},
   {path: '/view', component: View},
   { path: '/settings', component: Settings},
-  {path: '/create', component: Create}
+  {path: '/create', component: Create},
+
+  // { path: '/:pathMatch(.*)*', redirect: '/login' } // Catch-all route to handle undefined routes
+  
 
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+
+router.beforeEach((to, from, next) => {
+  console.log('Attempting to navigate to:', to.path);
+  next();
 });
 
 export default router;

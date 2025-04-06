0d1f3960d79832319bea88afd15bf1f4af9164e9
jomal <johnj18@farmingdale.edu>
2025-04-05 23:09:45 -0400

    - Added CSS styling to radio button on QuizQuestion.vue

--- src/components/QuizQuestion.vue
+++ src/components/QuizQuestion.vue
@@ -2,21 +2,24 @@
   <div class="question-box">
     <p>{{ question.text }}</p>
 
-    <div v-if="mode === 'Multiple Choice'">
-      <button
+    <div v-if="mode === 'Multiple Choice' || mode === 'True/False'">
+      <label
           v-for="(opt, index) in question.options"
           :key="index"
-          :disabled="answered"
-          @click="checkAnswer(opt)"
+          class="radio-option"
       >
+        <input
+            type="radio"
+            :value="opt"
+            v-model="userAnswer"
+            :disabled="answered"
+            @change="checkAnswer(opt)"
+        />
+        <span class="custom-radio"></span>
         {{ opt }}
-      </button>
+      </label>
     </div>
 
-    <div v-else-if="mode === 'True/False'">
-      <button :disabled="answered" @click="checkAnswer('True')">True</button>
-      <button :disabled="answered" @click="checkAnswer('False')">False</button>
-    </div>
 
     <div v-else-if="mode === 'One-word'">
       <input type="text" v-model="userAnswer" :disabled="answered" />
@@ -64,4 +67,53 @@ export default {
   margin-right: 10px;
   margin-top: 10px;
 }
+.radio-option {
+  display: flex;
+  align-items: center;
+  background: #1e2238;
+  padding: 14px 18px;
+  margin: 10px 0;
+  border-radius: 10px;
+  font-weight: 500;
+  cursor: pointer;
+  position: relative;
+  color: white;
+  border: 2px solid transparent;
+  transition: border-color 0.3s ease;
+}
+
+.radio-option input[type="radio"] {
+  display: none;
+}
+
+.custom-radio {
+  height: 18px;
+  width: 18px;
+  border-radius: 50%;
+  border: 2px solid #5f98ef;
+  margin-right: 14px;
+  position: relative;
+  flex-shrink: 0;
+}
+
+input[type="radio"]:checked + .custom-radio {
+  background-color: #5f98ef;
+  border-color: #5f98ef;
+}
+
+input[type="radio"]:checked + .custom-radio::after {
+  content: "";
+  position: absolute;
+  top: 3px;
+  left: 3px;
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+}
+
+.radio-option:hover {
+  background: #1e2238;
+}
+
+
 </style>

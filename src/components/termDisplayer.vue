bb163320aa79ebf8e6b5f1d24ae89316b47ea227
Chris <marac@farmingdale.edu>
2025-04-22 14:06:59 -0400

    updated termdisplayer

--- src/components/termDisplayer.vue
+++ src/components/termDisplayer.vue
@@ -28,29 +28,29 @@
   <style scoped>
   .definition-row {
     display: flex;
-    width: 100%; /* Occupy full width of its container */
-    margin-bottom: 10px; /* Space between rows */
+    width: 100%; 
+    margin-bottom: 10px; 
     border-radius: 10px;
-    overflow: hidden; /* To contain rounded corners of inner divs */
+    overflow: hidden; 
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
   }
   
   .term-container,
   .definition-container {
-    flex: 1; /* Distribute space equally */
+    flex: 1;
     background: var(--Black, #121729);
     color: white;
     padding: 15px;
     box-sizing: border-box;
     font-family: 'Outfit', sans-serif;
-    font-size: 1.1rem; /* Adjust font size as needed */
+    font-size: 1.1rem; 
     display: flex;
     align-items: center;
-    min-height: 50px; /* Minimum height for each row */
+    min-height: 50px; 
   }
   
   .term-container {
-    border-right: 1px solid #22293a; /* Separator between term and definition */
+    border-right: 1px solid #22293a; 
     text-align: left;
   }
   
